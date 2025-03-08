58b666c118d87a14f53282b561b1b218792d82e6
nada-03 <nadd45567@gmail.com>
2025-03-08 18:36:25 -0500

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -53,8 +53,19 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
+    const title = req.params.title; // Retrieve title from request parameters
+    const booksByTitle = Object.values(books).filter(b => b.title === title); // Convert books to an array and filter by title
+
+    if (booksByTitle.length > 0) {
+        // Return the list of books by the title as a JSON response
+        res.status(200).json(booksByTitle);
+    } else {
+        // Return a 404 error if no books are found for the title
+        res.status(404).json({ message: "No books found for the specified title" });
+    }
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+
 });
 
 //  Get book review
